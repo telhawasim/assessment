4ea667336865b359785ee58c0e3273cf648b2763
Telha Wasim <telhawasim@gmail.com>
2024-10-24 13:16:29 +0500

    project setup

---
+++ Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assessment
+//
+//  Created by Telha Wasim on 24/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
